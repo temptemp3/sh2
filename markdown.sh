83e9a91c9bcf90d5528d16b8f80cc4c7a6c494df
temptemp3 <temptemp3@users.noreply.github.com>
2017-06-24 17:38:57 +0900

    Update markdown.sh

--- markdown.sh
+++ markdown.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 ## markdown 
 ## - pl markdown wrapper
-## version 0.0.2 - fix @ zenkaku space
+## version 0.1.0 - exit on error, get-markdown-command
 ## =to do=
 ## - check for markdown command, mc
 ##  + use mc, primary
 ## - check for perl
 ##  + install Markdown.pl as mc
 ##     + use mc, secondary
-## - allow override
+## - allow override, may require entry modification
 ##  + use mc provided with interface, optional
+##################################################
+set -e # exit on error
 ##################################################
 markdown() {
  test ! "${infile}" || {
@@ -20,8 +22,46 @@ markdown() {
  }
 }
 #-------------------------------------------------
+test-markdown-command() {
+ command markdown -V 2>/dev/null && {
+  echo markdown
+ }
+}
+#-------------------------------------------------
+test-perl() {
+ perl -v 2>/dev/null && {
+ true
+}
+#-------------------------------------------------
+test-markdown-global() {
+ true
+ # 1. return markdown if installed else
+ # 1. download <http://daringfireball.net/projects/downloads/Markdown_1.0.1.zip>, it
+ # 1. test PL, if _ does not exist install _ in user home
+ # 1. put it in PL
+ # 1. install it
+}
+#-------------------------------------------------
+test-markdown-global-command() {
+ perl ${markdown} --version 2>/dev/null && {
+  echo perl ${markdown}
+ }
+}
+#-------------------------------------------------
+markdown_command=
+get-markdown-command() {
+ markdown_command=$( 
+  test-markdown-command ||
+  test-perl || 
+  test-markdown-global ||
+  test-markdown-global-command ||
+  false
+ )
+ test "${markdown_command}" 
+}
+#-------------------------------------------------
 convert-infile() {
- perl ${PL}/Markdown.pl ${infile} # use 
+ perl ${PL}/Markdown.pl ${infile} # use Markdown.pl
 }
 #-------------------------------------------------
 convert-input() { 
