091982a029054080b4548fc237a80159dc69a774
Nicholas M Shellabarger <nicholas@h-maneki.net>
2018-01-08 22:39:19 +0900

    Merge remote-tracking branch 'origin/sh2' into test

--- range.sh
+++ range.sh
@@ -1,49 +1,56 @@
 #!/bin/bash
 ## range
 ## - returns range of numbers
-## version 0.1.1 - test-range lower bound update ge zero
+## version 0.1.3 - source safe
 #####################################################################################
-. error.sh	# error hangling
-error "true"	# show errors
+. $( dirname ${0} )/error.sh
+error "true"
 #####################################################################################
-eval-range() {
- eval echo {${range_start}..${range_end}..${range_incr}}
-}
-#------------------------------------------------------------------------------------
-test-range() {
- test ${range_start} -ge 0 -a ${range_end} -ge ${range_start} || {
-  error "out of range" "${BASHFUNC}" "${LINENO}"
-  false
- }
-}
-#------------------------------------------------------------------------------------
-range-list() {
- test-range
- eval-range
-}
-#------------------------------------------------------------------------------------
 range() {
- range-list
+ eval-range() {
+  eval echo {${range_start}..${range_end}..${range_incr}}
+ }
+ #------------------------------------------------------------------------------------
+ test-range() {
+  test ${range_start} -ge 0 -a ${range_end} -ge ${range_start} || {
+   error "out of range" "${BASHFUNC}" "${LINENO}"
+   false
+  }
+ }
+ #------------------------------------------------------------------------------------
+ range-list() {
+  test-range
+  eval-range
+ }
+ #------------------------------------------------------------------------------------
+ range() {
+  range-list
+ }
+ #####################################################################################
+ if [ ${#} -eq 3 ]
+ then
+  range_start=${1}
+  range_end=${2}
+  range_incr=${3}
+ elif [ ${#} -eq 2 ]
+ then
+  range_start=${1}
+  range_end=${2}
+  range_incr=1
+ elif [ ${#} -eq 1 ] 
+ then
+  range_start=1
+  range_end=${1}
+  range_incr=1
+ elif [ ${#} -eq 0 ]
+ then
+  exit 0 # no args, do nothing
+ else
+  exit 1 # wrong args
+ fi
+ #####################################################################################
+ range
 }
 #####################################################################################
-if [ ${#} -eq 3 ]
-then
- range_start=${1}
- range_end=${2}
- range_incr=${3}
-elif [ ${#} -eq 2 ]
-then
- range_start=${1}
- range_end=${2}
- range_incr=1
-elif [ ${#} -eq 1 ] 
-then
- range_start=1
- range_end=${1}
- range_incr=1
-else
- exit 1 # wrong args
-fi
-#####################################################################################
-range
+range ${@}
 #####################################################################################
