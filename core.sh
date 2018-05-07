45fe0c2dd1770f10894eefb56c80039be43bd7b2
Nicholas M Shellabarger <nicholas@h-maneki.net>
2018-05-07 22:19:14 +0900

    cleanup

--- core.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-## core - core scripts
-## version 0.0.1 - initial, import
-##################################################
-. ${SH}/attr.sh 	# attribute
-. ${SH}/set.sh		# shell options
-set_exit_on_error	# (default) options
-. ${SH}/echo.sh		# _echo
-##################################################
